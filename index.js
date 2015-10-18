8cc2536a2ed00f6f43d3100f29e26328c2989e14
JD Isaacks <jd@jisaacks.com>
2015-10-18 08:58:32 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var entryPoint = require.resolve('font-awesome');
+
+var faDir = path.dirname(entryPoint);
+
+var pkg = path.join(faDir, 'package.json');
+
+// SASS/LESS directory for import paths
+var scssDir = path.join(faDir, 'scss');
+var lessDir = path.join(faDir, 'less');
+
+// Direct path to fontawesome.css
+var cssPath = path.join(faDir, pkg.style);
+
+// Glob of all files in fonts directory
+var fonts = path.join(faDir, 'fonts', '*');
+
+module.exports = {
+
+  scssPath: scssDir,
+  lessPath: lessDir,
+  css: cssPath,
+  
+  fonts: fonts
+
+};
